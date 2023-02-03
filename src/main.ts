19a5a2f3df53a5e5edc758cd5db6934081c63d63
aless133 <aless133@yandex.ru>
2023-02-03 13:02:31 +0300

    feat: add dotenv.config

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
@@ -5,6 +8,6 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true, forbidNonWhitelisted: true }));
-  await app.listen(4000);
+  await app.listen(parseInt((process.env.PORT || "4000") as string));
 }
 bootstrap();
